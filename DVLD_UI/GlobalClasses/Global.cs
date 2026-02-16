eba3fe3038932faeceb85308c2897b073ef33b62
abdalbaset <abdallbaset2004@gmail.com>
2026-02-16 22:21:17 +0330

    refactor(ui): empty Global class after migrating image logic to Util

--- DVLD_UI/GlobalClasses/Global.cs
+++ DVLD_UI/GlobalClasses/Global.cs
@@ -8,20 +8,7 @@ namespace DVLD_UI.GlobalClasses
 {
     static class clsGlobal
     {
-       static public void DeletePersonImageOnDisk(string FilePath)
-        {
-
-            try
-            {
-                if (!string.IsNullOrEmpty(FilePath) && File.Exists(FilePath))
-                    File.Delete(FilePath);
-            }
-            catch (Exception ex)
-            {
-                //Errors will be recorded in the LOG file later.
-            }
-
-        }
+       
 
  
     }
