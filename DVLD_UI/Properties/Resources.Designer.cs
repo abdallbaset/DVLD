ef414fbcf04a28c3ac42f1989c63fdaf1497af92
abdalbaset <abdallbaset2004@gmail.com>
2026-02-16 22:21:46 +0330

    chore: update UI resources and designer files

--- DVLD_UI/Properties/Resources.Designer.cs
+++ DVLD_UI/Properties/Resources.Designer.cs
@@ -173,6 +173,16 @@ namespace DVLD_UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap password {
+            get {
+                object obj = ResourceManager.GetObject("password", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap people {
             get {
                 object obj = ResourceManager.GetObject("people", resourceCulture);
