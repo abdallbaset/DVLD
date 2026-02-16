14f6a4b4d8af8d6c4ca07139f8e2b10a96dfc23e
abdalbaset <abdallbaset2004@gmail.com>
2026-02-16 22:14:08 +0330

    feat(ui): add Utility and Validation classes and migrate image deletion logic

---
+++ DVLD_UI/GlobalClasses/Validatoin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace DVLD_UI.GlobalClasses
+{
+    static public  class clsValidatoin
+    {
+        static public bool ValidateEmail(string EmailAddress)
+        {
+            if (string.IsNullOrWhiteSpace(EmailAddress))
+                return false;
+
+
+            string pattern = @"^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+
+            return Regex.IsMatch(EmailAddress, pattern);
+        }
+    }
+}
