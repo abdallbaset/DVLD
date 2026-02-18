48bd06ef00547ff85a1d6565fc1bfb44e4ba7130
abdalbaset <abdallbaset2004@gmail.com>
2026-02-18 11:11:18 +0330

    feat(ui): add ctrlUserCard UserControl for displaying user information

---
+++ DVLD_UI/Users/ctrlUserCard.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace DVLD_UI.Users
+{
+    partial class ctrlUserCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_IsActive = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_UserName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_UserID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctrlPersonCard1 = new DVLD_UI.ctrlPersonCard();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_IsActive);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lbl_UserName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lbl_UserID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(5, 371);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1119, 111);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login Information";
+            // 
+            // lbl_IsActive
+            // 
+            this.lbl_IsActive.AutoSize = true;
+            this.lbl_IsActive.Location = new System.Drawing.Point(948, 50);
+            this.lbl_IsActive.Name = "lbl_IsActive";
+            this.lbl_IsActive.Size = new System.Drawing.Size(43, 24);
+            this.lbl_IsActive.TabIndex = 5;
+            this.lbl_IsActive.Text = "???";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(842, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IsActive:";
+            // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.AutoSize = true;
+            this.lbl_UserName.Location = new System.Drawing.Point(605, 50);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(43, 24);
+            this.lbl_UserName.TabIndex = 3;
+            this.lbl_UserName.Text = "???";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(472, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User Name:";
+            // 
+            // lbl_UserID
+            // 
+            this.lbl_UserID.AutoSize = true;
+            this.lbl_UserID.Location = new System.Drawing.Point(229, 50);
+            this.lbl_UserID.Name = "lbl_UserID";
+            this.lbl_UserID.Size = new System.Drawing.Size(43, 24);
+            this.lbl_UserID.TabIndex = 1;
+            this.lbl_UserID.Text = "???";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User ID:";
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ctrlPersonCard1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(5, 4);
+            this.ctrlPersonCard1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(1119, 360);
+            this.ctrlPersonCard1.TabIndex = 0;
+            // 
+            // ctrlUserCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Name = "ctrlUserCard";
+            this.Size = new System.Drawing.Size(1132, 492);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlPersonCard ctrlPersonCard1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_UserID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_IsActive;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ DVLD_UI/Users/ctrlUserCard.cs
@@ -0,0 +1,47 @@
+﻿using DVLD_Business;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Users
+{
+    public partial class ctrlUserCard : UserControl
+    {
+        public ctrlUserCard()
+        {
+            InitializeComponent();
+        }
+        private void _LoadDefaultData()
+        {
+            lbl_UserID.Text = "???";
+            lbl_UserName.Text = "???";
+            lbl_IsActive.Text = "???";
+            ctrlPersonCard1.LoadDefaultData();
+        }
+        public void LoadUserInfo(int UserID)
+        {
+            clsUser User = clsUser.FindByUserID(UserID);
+            if (User == null)
+            {
+                _LoadDefaultData();
+                MessageBox.Show($"No User with ID [{UserID}] was found in the system.",
+                                     "Not Found",
+                                     MessageBoxButtons.OK,
+                                     MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            ctrlPersonCard1.LoadPersonInfo(User.UserInfo.PersonID);
+
+            lbl_UserID.Text = User.UserInfo.UserID.ToString();
+            lbl_UserName.Text = User.UserInfo.UserName;
+            lbl_IsActive.Text = (User.UserInfo.IsActive) ? "Yes" : "No";
+        }
+    }
+}
