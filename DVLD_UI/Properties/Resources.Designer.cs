924b0a4a5777b5698f4f3a01a19b6a8fff0d94c2
abdalbaset <abdallbaset2004@gmail.com>
2026-02-22 11:27:24 +0330

    feat(resources): add sign_out icon to project resources

--- DVLD_UI/Properties/Resources.Designer.cs
+++ DVLD_UI/Properties/Resources.Designer.cs
@@ -323,6 +323,16 @@ namespace DVLD_UI.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap sign_out {
+            get {
+                object obj = ResourceManager.GetObject("sign-out", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap users {
             get {
                 object obj = ResourceManager.GetObject("users", resourceCulture);
