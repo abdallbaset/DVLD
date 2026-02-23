be3b1ca7b6db31a2dcacb0c8e2c6d5f30e292b56
abdalbaset <abdallbaset2004@gmail.com>
2026-02-23 14:16:17 +0330

    feat(ui): implement login logic and session initialization in frmLogin

---
+++ DVLD_UI/Login/frmLogin.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace DVLD_UI.Login
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_PassWord = new System.Windows.Forms.TextBox();
+            this.ckb_RememberMe = new System.Windows.Forms.CheckBox();
+            this.btn_Login = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ptcb_Close = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ptcb_Close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(479, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 40);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Sign In";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label2.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(481, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(457, 29);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Enter your credentials to access the administrative system";
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Font = new System.Drawing.Font("Sitka Banner", 12F);
+            this.txt_UserName.Location = new System.Drawing.Point(486, 175);
+            this.txt_UserName.Multiline = true;
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(452, 39);
+            this.txt_UserName.TabIndex = 10;
+            this.txt_UserName.Tag = "User Name";
+            this.txt_UserName.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Box_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(481, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Username";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(481, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 29);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Password";
+            // 
+            // txt_PassWord
+            // 
+            this.txt_PassWord.Font = new System.Drawing.Font("Sitka Banner", 12F);
+            this.txt_PassWord.Location = new System.Drawing.Point(486, 266);
+            this.txt_PassWord.Multiline = true;
+            this.txt_PassWord.Name = "txt_PassWord";
+            this.txt_PassWord.Size = new System.Drawing.Size(452, 39);
+            this.txt_PassWord.TabIndex = 12;
+            this.txt_PassWord.Tag = "PassWord";
+            this.txt_PassWord.Validating += new System.ComponentModel.CancelEventHandler(this.txt_Box_Validating);
+            // 
+            // ckb_RememberMe
+            // 
+            this.ckb_RememberMe.AutoSize = true;
+            this.ckb_RememberMe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ckb_RememberMe.Font = new System.Drawing.Font("Sitka Banner", 12F);
+            this.ckb_RememberMe.Location = new System.Drawing.Point(486, 323);
+            this.ckb_RememberMe.Name = "ckb_RememberMe";
+            this.ckb_RememberMe.Size = new System.Drawing.Size(149, 33);
+            this.ckb_RememberMe.TabIndex = 14;
+            this.ckb_RememberMe.Text = "Remember me ";
+            this.ckb_RememberMe.UseVisualStyleBackColor = true;
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(127)))), ((int)(((byte)(236)))));
+            this.btn_Login.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.Font = new System.Drawing.Font("Sitka Text", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Login.ForeColor = System.Drawing.Color.White;
+            this.btn_Login.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Login.ImageIndex = 0;
+            this.btn_Login.ImageList = this.imageList1;
+            this.btn_Login.Location = new System.Drawing.Point(486, 408);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(452, 57);
+            this.btn_Login.TabIndex = 15;
+            this.btn_Login.Text = "Login";
+            this.btn_Login.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Login.UseVisualStyleBackColor = false;
+            this.btn_Login.Click += new System.EventHandler(this.btn_Login_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "login.png");
+            // 
+            // ptcb_Close
+            // 
+            this.ptcb_Close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ptcb_Close.Image = global::DVLD_UI.Properties.Resources.close_square;
+            this.ptcb_Close.Location = new System.Drawing.Point(973, 13);
+            this.ptcb_Close.Margin = new System.Windows.Forms.Padding(4);
+            this.ptcb_Close.Name = "ptcb_Close";
+            this.ptcb_Close.Size = new System.Drawing.Size(40, 39);
+            this.ptcb_Close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptcb_Close.TabIndex = 7;
+            this.ptcb_Close.TabStop = false;
+            this.ptcb_Close.Click += new System.EventHandler(this.ptcb_Close_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::DVLD_UI.Properties.Resources.License_Department;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(393, 562);
+            this.panel1.TabIndex = 0;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(1033, 559);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.ckb_RememberMe);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_PassWord);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_UserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ptcb_Close);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptcb_Close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox ptcb_Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_PassWord;
+        private System.Windows.Forms.CheckBox ckb_RememberMe;
+        private System.Windows.Forms.Button btn_Login;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ DVLD_UI/Login/frmLogin.cs
@@ -0,0 +1,142 @@
+﻿using DVLD_Business;
+using DVLD_UI.GlobalClasses;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace DVLD_UI.Login
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void ptcb_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void _HandleRememberMe()
+        {
+            string userName = txt_UserName.Text.Trim();
+            string password = clsGlobal.Encrypt(txt_PassWord.Text.Trim());
+            try
+            {
+                if (ckb_RememberMe.Checked)
+                {
+                    Registry.SetValue(clsGlobal.KeyPath, "UserName", userName);
+                    Registry.SetValue(clsGlobal.KeyPath, "Password", password);
+                }
+                else
+                {
+                    Registry.SetValue(clsGlobal.KeyPath, "UserName", "");
+                    Registry.SetValue(clsGlobal.KeyPath, "Password", "");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Errors will be recorded in the LOG file later.
+
+            }
+        }
+
+        private void _RetrieveRememberedData()
+        {
+            try
+            {
+                string savedUser = Registry.GetValue(clsGlobal.KeyPath, "UserName", string.Empty) as string;
+                string savedPass = Registry.GetValue(clsGlobal.KeyPath, "Password", string.Empty) as string;
+
+                if (!string.IsNullOrEmpty(savedUser) &&  !string.IsNullOrEmpty(savedPass))
+                {
+                   
+                    txt_UserName.Text = savedUser;
+                    txt_PassWord.Text = clsGlobal.Decrypt(savedPass);
+                    ckb_RememberMe.Checked = true;
+                }
+            }
+            catch
+            {                 // Errors will be recorded in the LOG file later.
+            }
+        }
+        private bool _IsUserLoginSuccessful()
+        {
+           clsGlobal.CurrentUser = clsUser.FindByUserameAndPassword(txt_UserName.Text.Trim(), txt_PassWord.Text.Trim());
+            return clsGlobal.CurrentUser != null;
+        }
+
+       private  bool _IsUserActive()
+        {
+            return clsGlobal.CurrentUser.UserInfo.IsActive;
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            if (!this.ValidateChildren())
+            {
+                MessageBox.Show("Some fileds are not valide!, put the mouse over the red icon(s) to see the erro",
+                   "Validation Error",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (_IsUserLoginSuccessful())
+            {
+                if (_IsUserActive())
+                {
+                    this.DialogResult = DialogResult.OK;
+                    _HandleRememberMe();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Your account is not active, please contact the administrator",
+                        "Account Inactive",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid username or password, please try again",
+                    "Login Failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void txt_Box_Validating(object sender, CancelEventArgs e)
+        {
+            TextBox txtBox = sender as TextBox;
+            if (txtBox != null)
+            {
+                if (string.IsNullOrEmpty(txtBox.Text))
+                {
+                    errorProvider1.SetError(txtBox, $"{txtBox.Tag} is required.");
+                    e.Cancel = true;
+                }
+                else
+                {
+                    errorProvider1.SetError(txtBox, string.Empty);
+                }
+            }
+
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            _RetrieveRememberedData();
+        }
+    }
+}
