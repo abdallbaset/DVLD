1b57a8b3a05df4c768aa7d51fc6282016007c0ec
abdalbaset <abdallbaset2004@gmail.com>
2026-02-24 12:16:13 +0330

    refactor: rename Controls folder to Applications and add ApplicationTypes subfolder with frmManageApplicationTypes

---
+++ DVLD_UI/Applications/Application Types/frmManageApplicationTypes.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace DVLD_UI.Applications.Application_Types
+{
+    partial class frmManageApplicationTypes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManageApplicationTypes));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbl_NumberOfRecords = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.dgv_ListApplication = new System.Windows.Forms.DataGridView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ListApplication)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(6, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(966, 56);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Manage Application Types";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DVLD_UI.Properties.Resources.Application_Types_512;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(971, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbl_NumberOfRecords
+            // 
+            this.lbl_NumberOfRecords.AutoSize = true;
+            this.lbl_NumberOfRecords.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lbl_NumberOfRecords.Location = new System.Drawing.Point(126, 725);
+            this.lbl_NumberOfRecords.Name = "lbl_NumberOfRecords";
+            this.lbl_NumberOfRecords.Size = new System.Drawing.Size(19, 24);
+            this.lbl_NumberOfRecords.TabIndex = 13;
+            this.lbl_NumberOfRecords.Text = "?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 725);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "# Records:";
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Close.ImageIndex = 1;
+            this.btn_Close.ImageList = this.imageList1;
+            this.btn_Close.Location = new System.Drawing.Point(832, 716);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(123, 45);
+            this.btn_Close.TabIndex = 10;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            // 
+            // dgv_ListApplication
+            // 
+            this.dgv_ListApplication.AllowUserToAddRows = false;
+            this.dgv_ListApplication.AllowUserToDeleteRows = false;
+            this.dgv_ListApplication.AllowUserToOrderColumns = true;
+            this.dgv_ListApplication.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_ListApplication.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_ListApplication.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv_ListApplication.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ListApplication.Location = new System.Drawing.Point(19, 272);
+            this.dgv_ListApplication.Name = "dgv_ListApplication";
+            this.dgv_ListApplication.ReadOnly = true;
+            this.dgv_ListApplication.RowHeadersWidth = 51;
+            this.dgv_ListApplication.RowTemplate.Height = 26;
+            this.dgv_ListApplication.Size = new System.Drawing.Size(942, 438);
+            this.dgv_ListApplication.TabIndex = 9;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "person-add.png");
+            this.imageList1.Images.SetKeyName(1, "close.png");
+            // 
+            // frmManageApplicationTypes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(973, 797);
+            this.Controls.Add(this.lbl_NumberOfRecords);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.dgv_ListApplication);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmManageApplicationTypes";
+            this.Text = "Manage Application Types";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ListApplication)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_NumberOfRecords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.DataGridView dgv_ListApplication;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
