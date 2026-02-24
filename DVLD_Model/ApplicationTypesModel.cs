554e3aa76a4ada2f6d570f03a915cd36dbea8b14
abdalbaset <abdallbaset2004@gmail.com>
2026-02-24 14:42:11 +0330

    feat(model): add clsApplicationTypesModel to DVLD_Model layer

---
+++ DVLD_Model/ApplicationTypesModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Model
+{
+    public class clsApplicationTypesModel
+    {
+        public int ApplicationTypeID { get; set; } = -1;
+        public string ApplicationTypeTitle { get; set; } = string.Empty;
+        public double ApplicationFees { get; set; } = 0;
+
+    }
+}
