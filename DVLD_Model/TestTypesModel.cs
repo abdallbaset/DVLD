5ccb0fccf696d5c687b2963ec13e6481d143a616
abdalbaset <abdallbaset2004@gmail.com>
2026-02-26 10:13:22 +0330

    feat(models): add clsTestTypesModel to represent test type data

---
+++ DVLD_Model/TestTypesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Model
+{
+    public class clsTestTypesModel
+    {
+       public int TestTypeID { get; set; } = -1;
+       public string TestTypeTitle { get; set; } = string.Empty;
+       public string TestTypeDescription { get; set;} = string.Empty;
+       public double TestTypeFees { get; set;} = 0;
+
+     
+
+    }
+}
