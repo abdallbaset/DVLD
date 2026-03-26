932c0d0ee0ba38f163eb5f3a23fc3fbd5769a767
abdalbaset <abdallbaset2004@gmail.com>
2026-03-26 15:17:53 +0330

    feat(model): add clsLicenseModel to encapsulate license data

---
+++ DVLD_Model/LicenseModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Model
+{
+    public class clsLicenseModel
+    {
+        public enum enIssueReason : byte
+        {
+            FirstTime = 1,
+            Renew = 2,
+            ReplacementForDamaged = 3,
+            ReplacementForLost = 4
+        }
+
+        public int LicenseID { get; set; } = (int)clsSettingsModel.enIdentityStatus.NonExistent;
+        public int LicenseClassID { get; set; } = (int)clsSettingsModel.enIdentityStatus.NonExistent;
+        public int ApplicationID { get; set; } = (int)clsSettingsModel.enIdentityStatus.NonExistent;
+        public int DriverID { get; set; } = (int)clsSettingsModel.enIdentityStatus.NonExistent;
+        public DateTime IssueDate { get; set; } = DateTime.Now;
+        public DateTime ExpirationDate { get; set; } = DateTime.Now;
+        public enIssueReason IssueReason { get; set; } = enIssueReason.FirstTime;
+        public double PaidFees { get; set; } = 0;
+        public bool IsActive { get; set; } = true;
+        public string Notes { get; set; } = string.Empty;
+        public int CreatedByUserID1 { get; set; } = (int)clsSettingsModel.enIdentityStatus.NonExistent;
+
+
+
+
+
+}
+}
