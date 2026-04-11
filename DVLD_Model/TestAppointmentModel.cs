bf809504dce6a3f6f5a01c880c5cb13e5f2760d4
abdalbaset <abdallbaset2004@gmail.com>
2026-04-11 14:06:56 +0330

    feat(model): add TestAppointmentModel for scheduling data

---
+++ DVLD_Model/TestAppointmentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Model
+{
+    public class clsTestAppointmentModel
+    {
+        public int  TestAppointmentID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public  int LocalDrivingLicenseApplicationID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public int TestTypeID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public DateTime AppointmentDate { get; set; } = DateTime.MinValue;
+        public double PaidFees { get; set; } = 0;
+        public int CreatedByUserID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public bool IsLocked { get; set; } = false;
+        public int RetakeTestApplicationID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+    }
+}
