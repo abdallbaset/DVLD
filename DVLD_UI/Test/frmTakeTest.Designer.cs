22ec745ea607d23027f108cc81ee47b04d50d883
abdalbaset <abdallbaset2004@gmail.com>
2026-04-11 16:12:59 +0330

    design(ui): add frmTakeTest layout (UI only)
    Changes in this commit:
    
    Form Design: Created the initial design for frmTakeTest, including the layout for result selection and notes entry.
    
    Control Hosting: Placed the ctrlScheduledTest inside the form to visualize how appointment data will be displayed.
    
    Input Elements: Added RadioButtons for "Pass/Fail" results and a TextBox for "Notes", along with "Save" and "Close" buttons.
    
    Status: No business logic, validation, or database interaction has been implemented yet; this is a pure UI/UX skeleton commit.

---
+++ DVLD_UI/Test/frmTakeTest.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace DVLD_UI.Test
+{
+    partial class frmTakeTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTakeTest));
+            this.rb_Fail = new System.Windows.Forms.RadioButton();
+            this.rb_Pass = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_Notes = new System.Windows.Forms.TextBox();
+            this.lbl_UserMessage = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ctrlSecheduledTest1 = new DVLD_UI.Licenses.Local_Licenses.Controls.ctrlSecheduledTest();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rb_Fail
+            // 
+            this.rb_Fail.AutoSize = true;
+            this.rb_Fail.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Fail.Location = new System.Drawing.Point(217, 528);
+            this.rb_Fail.Name = "rb_Fail";
+            this.rb_Fail.Size = new System.Drawing.Size(62, 28);
+            this.rb_Fail.TabIndex = 197;
+            this.rb_Fail.Text = "Fail";
+            this.rb_Fail.UseVisualStyleBackColor = true;
+            // 
+            // rb_Pass
+            // 
+            this.rb_Pass.AutoSize = true;
+            this.rb_Pass.Checked = true;
+            this.rb_Pass.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rb_Pass.Location = new System.Drawing.Point(140, 527);
+            this.rb_Pass.Name = "rb_Pass";
+            this.rb_Pass.Size = new System.Drawing.Size(71, 28);
+            this.rb_Pass.TabIndex = 196;
+            this.rb_Pass.TabStop = true;
+            this.rb_Pass.Text = "Pass";
+            this.rb_Pass.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 530);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 24);
+            this.label1.TabIndex = 200;
+            this.label1.Text = "Result :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 570);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 24);
+            this.label4.TabIndex = 205;
+            this.label4.Text = "Notes : ";
+            // 
+            // txt_Notes
+            // 
+            this.txt_Notes.Location = new System.Drawing.Point(147, 570);
+            this.txt_Notes.Multiline = true;
+            this.txt_Notes.Name = "txt_Notes";
+            this.txt_Notes.Size = new System.Drawing.Size(360, 106);
+            this.txt_Notes.TabIndex = 206;
+            // 
+            // lbl_UserMessage
+            // 
+            this.lbl_UserMessage.AutoSize = true;
+            this.lbl_UserMessage.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_UserMessage.ForeColor = System.Drawing.Color.Red;
+            this.lbl_UserMessage.Location = new System.Drawing.Point(121, 773);
+            this.lbl_UserMessage.Name = "lbl_UserMessage";
+            this.lbl_UserMessage.Size = new System.Drawing.Size(270, 21);
+            this.lbl_UserMessage.TabIndex = 207;
+            this.lbl_UserMessage.Text = "You cannot change the results";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "save.png");
+            this.imageList1.Images.SetKeyName(1, "close_2.png");
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.BackColor = System.Drawing.Color.Green;
+            this.btn_Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Save.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btn_Save.ForeColor = System.Drawing.Color.White;
+            this.btn_Save.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Save.ImageIndex = 0;
+            this.btn_Save.ImageList = this.imageList1;
+            this.btn_Save.Location = new System.Drawing.Point(405, 705);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(102, 46);
+            this.btn_Save.TabIndex = 208;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Save.UseVisualStyleBackColor = false;
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Crimson;
+            this.btn_Close.CausesValidation = false;
+            this.btn_Close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Close.ImageIndex = 1;
+            this.btn_Close.ImageList = this.imageList1;
+            this.btn_Close.Location = new System.Drawing.Point(256, 705);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(102, 46);
+            this.btn_Close.TabIndex = 209;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::DVLD_UI.Properties.Resources.Number_32;
+            this.pictureBox4.Location = new System.Drawing.Point(103, 530);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(31, 29);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 203;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DVLD_UI.Properties.Resources.Notes_32;
+            this.pictureBox1.Location = new System.Drawing.Point(103, 566);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 199;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ctrlSecheduledTest1
+            // 
+            this.ctrlSecheduledTest1.Location = new System.Drawing.Point(12, 12);
+            this.ctrlSecheduledTest1.Name = "ctrlSecheduledTest1";
+            this.ctrlSecheduledTest1.Size = new System.Drawing.Size(507, 514);
+            this.ctrlSecheduledTest1.TabIndex = 0;
+            // 
+            // frmTakeTest
+            // 
+            this.AcceptButton = this.btn_Save;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Close;
+            this.ClientSize = new System.Drawing.Size(538, 822);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.lbl_UserMessage);
+            this.Controls.Add(this.txt_Notes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.rb_Fail);
+            this.Controls.Add(this.rb_Pass);
+            this.Controls.Add(this.ctrlSecheduledTest1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmTakeTest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Take Test";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Licenses.Local_Licenses.Controls.ctrlSecheduledTest ctrlSecheduledTest1;
+        private System.Windows.Forms.RadioButton rb_Fail;
+        private System.Windows.Forms.RadioButton rb_Pass;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_Notes;
+        private System.Windows.Forms.Label lbl_UserMessage;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
