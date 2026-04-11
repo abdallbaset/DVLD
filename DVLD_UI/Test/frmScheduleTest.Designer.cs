47641e2b82f0ad59cbec23709b07428c1f3514ab
abdalbaset <abdallbaset2004@gmail.com>
2026-04-11 16:09:16 +0330

    feat(ui): add frmScheduleTest to handle test booking and retakes
    Changes in this commit:
    
    New Form: Created frmScheduleTest as the main window for scheduling or rescheduling driving tests (Vision, Theory, Street).
    
    UI Hosting: Integrated the ctrlScheduleTest user control to handle the scheduling logic and data display.
    
    Initialization: Implemented a constructor that accepts LocalDrivingLicenseApplicationID and TestTypeID to dynamically configure the form's mode.
    
    User Experience: Added a “Close” button and ensured the form updates the parent list (Refresh) upon successful saving of a new appointment.

---
+++ DVLD_UI/Test/frmScheduleTest.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DVLD_UI.Test
+{
+    partial class frmScheduleTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmScheduleTest));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.ctrlScheduleTest1 = new DVLD_UI.Test.Controls.ctrlScheduleTest();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "close_2.png");
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Crimson;
+            this.btn_Close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Close.ImageIndex = 0;
+            this.btn_Close.ImageList = this.imageList1;
+            this.btn_Close.Location = new System.Drawing.Point(22, 715);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(131, 44);
+            this.btn_Close.TabIndex = 18;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            // 
+            // ctrlScheduleTest1
+            // 
+            this.ctrlScheduleTest1.Location = new System.Drawing.Point(2, 2);
+            this.ctrlScheduleTest1.Name = "ctrlScheduleTest1";
+            this.ctrlScheduleTest1.Size = new System.Drawing.Size(562, 774);
+            this.ctrlScheduleTest1.TabIndex = 0;
+            // 
+            // frmScheduleTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Close;
+            this.ClientSize = new System.Drawing.Size(567, 786);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.ctrlScheduleTest1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmScheduleTest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Schedule Test";
+            this.Load += new System.EventHandler(this.frmScheduleTest_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Controls.ctrlScheduleTest ctrlScheduleTest1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ DVLD_UI/Test/frmScheduleTest.cs
@@ -0,0 +1,23 @@
+﻿using DVLD_Model;
+using System;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Test
+{
+    public partial class frmScheduleTest : Form
+    {
+       private  int _LocalDrivingLicenseApplicationID = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+       private int _TestTypeID = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public frmScheduleTest(int LocalDrivingLicenseApplicationID, int testTypeID)
+        {
+            InitializeComponent();
+            _LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+            _TestTypeID = testTypeID;
+        }
+
+        private void frmScheduleTest_Load(object sender, EventArgs e)
+        {
+            ctrlScheduleTest1.LoadScheduleTestInfo(_LocalDrivingLicenseApplicationID, _TestTypeID);
+        }
+    }
+}
