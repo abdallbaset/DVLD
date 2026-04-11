4dfbee8869b272745fbcf2480850faac788b3317
abdalbaset <abdallbaset2004@gmail.com>
2026-04-11 15:45:12 +0330

    feat(ui): add frmLocalDrivingLicenseApplicationInfo form
    Changes in this commit:
    
    New Form: Created frmLocalDrivingLicenseApplicationInfo to provide a dedicated window for viewing local driving license application details.
    
    UI Integration: Hosted the ctrlDrivingLicenseApplicationInfo user control within the form for a modular UI design.
    
    Functionality: Added a close button and standardized the form's appearance (border style, start position, and icon).
    
    Accessibility: Implemented a constructor that accepts LocalDrivingLicenseApplicationID to trigger the data loading process automatically.

---
+++ DVLD_UI/Applications/Local Driving License/frmLocalDrivingLicenseApplicationInfo.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace DVLD_UI.Applications.Local_Driving_License
+{
+    partial class frmLocalDrivingLicenseApplicationInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLocalDrivingLicenseApplicationInfo));
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.ctrlDrivingLicenseApplicationInfo1 = new DVLD_UI.Applications.Local_Driving_License.ctrlDrivingLicenseApplicationInfo();
+            this.SuspendLayout();
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Crimson;
+            this.btn_Close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Close.ImageIndex = 0;
+            this.btn_Close.ImageList = this.imageList1;
+            this.btn_Close.Location = new System.Drawing.Point(732, 528);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(118, 41);
+            this.btn_Close.TabIndex = 18;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "close_2.png");
+            // 
+            // ctrlDrivingLicenseApplicationInfo1
+            // 
+            this.ctrlDrivingLicenseApplicationInfo1.Location = new System.Drawing.Point(12, 26);
+            this.ctrlDrivingLicenseApplicationInfo1.Name = "ctrlDrivingLicenseApplicationInfo1";
+            this.ctrlDrivingLicenseApplicationInfo1.Size = new System.Drawing.Size(849, 495);
+            this.ctrlDrivingLicenseApplicationInfo1.TabIndex = 0;
+            // 
+            // frmLocalDrivingLicenseApplicationInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Close;
+            this.ClientSize = new System.Drawing.Size(872, 580);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.ctrlDrivingLicenseApplicationInfo1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmLocalDrivingLicenseApplicationInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Local Driving License Application Info";
+            this.Load += new System.EventHandler(this.frmLocalDrivingLicenseApplicationInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ctrlDrivingLicenseApplicationInfo ctrlDrivingLicenseApplicationInfo1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ DVLD_UI/Applications/Local Driving License/frmLocalDrivingLicenseApplicationInfo.cs
@@ -0,0 +1,34 @@
+﻿using DVLD_UI.GlobalClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Applications.Local_Driving_License
+{
+    public partial class frmLocalDrivingLicenseApplicationInfo : Form
+    {
+
+       private int _LocalDrivingLicenseApplicationID = (int)clsGlobal.enIdentityStatus.NonExistent;
+        public frmLocalDrivingLicenseApplicationInfo(int LocalDrivingLicenseApplicationID)
+        {
+            InitializeComponent();
+            _LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmLocalDrivingLicenseApplicationInfo_Load(object sender, EventArgs e)
+        {
+            ctrlDrivingLicenseApplicationInfo1.Load_LocalDrivingLicenseApplicationInfo(_LocalDrivingLicenseApplicationID);
+        }
+    }
+}
