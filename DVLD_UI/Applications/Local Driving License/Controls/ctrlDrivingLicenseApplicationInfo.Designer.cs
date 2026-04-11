7607832e69ca8639d47ac19bccd2ef8814f2e832
abdalbaset <abdallbaset2004@gmail.com>
2026-04-11 15:10:52 +0330

    feat(ui): add ctrlDrivingLicenseApplicationInfo user control
    Changes in this commit:
    
    UI Component: Developed ctrlDrivingLicenseApplicationInfo to display comprehensive details of local driving license applications.
    
    Composition: Integrated license class information and pass-test counts alongside basic application data.
    
    Data Binding: Implemented logic to populate the control using clsLocalDrivingLicenseApplications business object.
    
    UI Enhancement: Added a link/label to view person details directly from the application info view.

---
+++ DVLD_UI/Applications/Local Driving License/Controls/ctrlDrivingLicenseApplicationInfo.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace DVLD_UI.Applications.Local_Driving_License
+{
+    partial class ctrlDrivingLicenseApplicationInfo
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.llbl_ShowLicenseInfo = new System.Windows.Forms.LinkLabel();
+            this.lbl_PassedTests = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_AppliedFor = new System.Windows.Forms.Label();
+            this.lbl_LocalDrivingLicenseApplicationID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.ctrlApplicationBasicInfo1 = new DVLD_UI.Applications.ctrlApplicationBasicInfo();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox4);
+            this.groupBox1.Controls.Add(this.llbl_ShowLicenseInfo);
+            this.groupBox1.Controls.Add(this.lbl_PassedTests);
+            this.groupBox1.Controls.Add(this.pictureBox3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lbl_AppliedFor);
+            this.groupBox1.Controls.Add(this.lbl_LocalDrivingLicenseApplicationID);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pictureBox6);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(842, 170);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Driving License Application Info";
+            // 
+            // llbl_ShowLicenseInfo
+            // 
+            this.llbl_ShowLicenseInfo.AutoSize = true;
+            this.llbl_ShowLicenseInfo.Location = new System.Drawing.Point(590, 47);
+            this.llbl_ShowLicenseInfo.Name = "llbl_ShowLicenseInfo";
+            this.llbl_ShowLicenseInfo.Size = new System.Drawing.Size(196, 24);
+            this.llbl_ShowLicenseInfo.TabIndex = 1;
+            this.llbl_ShowLicenseInfo.TabStop = true;
+            this.llbl_ShowLicenseInfo.Text = "Show License Info";
+            this.llbl_ShowLicenseInfo.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llbl_ShowLicenseInfo_LinkClicked);
+            // 
+            // lbl_PassedTests
+            // 
+            this.lbl_PassedTests.AutoSize = true;
+            this.lbl_PassedTests.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_PassedTests.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_PassedTests.ImageIndex = 0;
+            this.lbl_PassedTests.Location = new System.Drawing.Point(207, 88);
+            this.lbl_PassedTests.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lbl_PassedTests.Name = "lbl_PassedTests";
+            this.lbl_PassedTests.Size = new System.Drawing.Size(21, 24);
+            this.lbl_PassedTests.TabIndex = 67;
+            this.lbl_PassedTests.Text = "0";
+            this.lbl_PassedTests.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 88);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 24);
+            this.label4.TabIndex = 65;
+            this.label4.Text = "Passed Tests :";
+            // 
+            // lbl_AppliedFor
+            // 
+            this.lbl_AppliedFor.AutoSize = true;
+            this.lbl_AppliedFor.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_AppliedFor.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_AppliedFor.ImageIndex = 0;
+            this.lbl_AppliedFor.Location = new System.Drawing.Point(264, 130);
+            this.lbl_AppliedFor.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lbl_AppliedFor.Name = "lbl_AppliedFor";
+            this.lbl_AppliedFor.Size = new System.Drawing.Size(37, 24);
+            this.lbl_AppliedFor.TabIndex = 60;
+            this.lbl_AppliedFor.Text = "???";
+            this.lbl_AppliedFor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_LocalDrivingLicenseApplicationID
+            // 
+            this.lbl_LocalDrivingLicenseApplicationID.AutoSize = true;
+            this.lbl_LocalDrivingLicenseApplicationID.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_LocalDrivingLicenseApplicationID.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lbl_LocalDrivingLicenseApplicationID.ImageIndex = 0;
+            this.lbl_LocalDrivingLicenseApplicationID.Location = new System.Drawing.Point(207, 47);
+            this.lbl_LocalDrivingLicenseApplicationID.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lbl_LocalDrivingLicenseApplicationID.Name = "lbl_LocalDrivingLicenseApplicationID";
+            this.lbl_LocalDrivingLicenseApplicationID.Size = new System.Drawing.Size(37, 24);
+            this.lbl_LocalDrivingLicenseApplicationID.TabIndex = 54;
+            this.lbl_LocalDrivingLicenseApplicationID.Text = "???";
+            this.lbl_LocalDrivingLicenseApplicationID.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 47);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 24);
+            this.label1.TabIndex = 53;
+            this.label1.Text = "D.L.App ID :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(8, 128);
+            this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(220, 24);
+            this.label10.TabIndex = 41;
+            this.label10.Text = "Applied For License :";
+            // 
+            // ctrlApplicationBasicInfo1
+            // 
+            this.ctrlApplicationBasicInfo1.Location = new System.Drawing.Point(3, 179);
+            this.ctrlApplicationBasicInfo1.Name = "ctrlApplicationBasicInfo1";
+            this.ctrlApplicationBasicInfo1.Size = new System.Drawing.Size(842, 300);
+            this.ctrlApplicationBasicInfo1.TabIndex = 6;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::DVLD_UI.Properties.Resources.id;
+            this.pictureBox4.Location = new System.Drawing.Point(161, 47);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(31, 29);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 68;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::DVLD_UI.Properties.Resources.PassedTests_32;
+            this.pictureBox3.Location = new System.Drawing.Point(161, 85);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(31, 29);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 66;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::DVLD_UI.Properties.Resources.License_Type_32;
+            this.pictureBox6.Location = new System.Drawing.Point(225, 125);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(31, 29);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 43;
+            this.pictureBox6.TabStop = false;
+            // 
+            // ctrlDrivingLicenseApplicationInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ctrlApplicationBasicInfo1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "ctrlDrivingLicenseApplicationInfo";
+            this.Size = new System.Drawing.Size(849, 481);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.LinkLabel llbl_ShowLicenseInfo;
+        private System.Windows.Forms.Label lbl_PassedTests;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_AppliedFor;
+        private System.Windows.Forms.Label lbl_LocalDrivingLicenseApplicationID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label10;
+        private ctrlApplicationBasicInfo ctrlApplicationBasicInfo1;
+    }
+}
---
+++ DVLD_UI/Applications/Local Driving License/Controls/ctrlDrivingLicenseApplicationInfo.cs
@@ -0,0 +1,95 @@
+﻿using DVLD_Business;
+using DVLD_Model;
+using DVLD_UI.GlobalClasses;
+using DVLD_UI.Licenses.Local_Licenses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Applications.Local_Driving_License
+{
+    public partial class ctrlDrivingLicenseApplicationInfo : UserControl
+    {
+        public ctrlDrivingLicenseApplicationInfo()
+        {
+            InitializeComponent();
+        }
+
+        private clsLocalDrivingLicenseApplications _LocalDrivingLicenseApplication;
+        private int _LocalDrivingLicenseApplicationID = (int)clsGlobal.enIdentityStatus.NonExistent;
+        public int LocalDrivingLicenseApplicationID
+        {
+            get => _LocalDrivingLicenseApplicationID;
+        }
+        public int PassedTestsCount
+        {
+            get => _PassedTestsCount;
+        }
+
+
+        private int _PassedTestsCount  = 0;
+
+        private int _LicenseID = (int)clsGlobal.enIdentityStatus.NonExistent;
+
+
+        private void _LoadDefaultData()
+        {
+            lbl_AppliedFor.Text = "???";
+            lbl_LocalDrivingLicenseApplicationID.Text = "???";
+            lbl_PassedTests.Text = "0";
+            ctrlApplicationBasicInfo1.LoadDefaultData();
+        }
+
+
+        private void _FillLocalDrivingLicenseApplicationInfo()
+        {
+            ctrlApplicationBasicInfo1.LoadApplicationInfo(_LocalDrivingLicenseApplication.ApplicationID);
+
+            lbl_LocalDrivingLicenseApplicationID.Text = LocalDrivingLicenseApplicationID.ToString();
+            lbl_AppliedFor.Text = _LocalDrivingLicenseApplication.ClassName;
+            lbl_PassedTests.Text = $"{_PassedTestsCount}/3";
+        }
+        public void Load_LocalDrivingLicenseApplicationInfo(int LocalDrivingLicenseApplicationID)
+        {
+            _PassedTestsCount = clsTests.GetTestPassedCount(LocalDrivingLicenseApplicationID);
+            _LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+            _LocalDrivingLicenseApplication = clsLocalDrivingLicenseApplications.FindByLocalDrivingLicenseApplicationID(_LocalDrivingLicenseApplicationID);
+            llbl_ShowLicenseInfo.Enabled = false;
+
+            if (_LocalDrivingLicenseApplication == null)
+            {
+                _LoadDefaultData();
+                MessageBox.Show($"No Local Driving License Application  with ID [{_LocalDrivingLicenseApplicationID}] was found in the system.",
+                                     "Not Found",
+                                     MessageBoxButtons.OK,
+                                     MessageBoxIcon.Exclamation);
+                return;
+            }
+
+             _HandleLicenseLinkState();
+            _FillLocalDrivingLicenseApplicationInfo();
+
+        }
+
+        private void llbl_ShowLicenseInfo_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmShowLicenseInfo frm = new frmShowLicenseInfo(_LicenseID);
+            frm.ShowDialog();
+        }
+        private void _HandleLicenseLinkState()
+        {
+             _LicenseID = _LocalDrivingLicenseApplication.GetActiveLicensesID();
+
+            llbl_ShowLicenseInfo.Enabled = (_LicenseID != (int)clsGlobal.enIdentityStatus.NonExistent) ;
+
+        }
+
+       
+    }
+}
