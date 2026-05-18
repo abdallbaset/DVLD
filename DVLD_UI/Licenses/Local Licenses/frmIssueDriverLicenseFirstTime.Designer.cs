2316f2e06790c646bba79acd80322bb9ddb23735
abdalbaset <abdallbaset2004@gmail.com>
2026-05-18 18:30:54 +0330

    feat: add frmIssueDriverLicenseFirstTime form with optimized validation on save
    Description:
    
    Created Issue License Form: Added frmIssueDriverLicenseFirstTime to the UI layer to handle the initial issuance of driving licenses for qualified applicants.
    
    Optimized UI Validation Flow: Positioned critical pre-issuance business validation checks (_IsPassedAllTests, _IsLicenseExist) inside the load event .
    
    Defensive Form Closure: Implemented programmatic safe exit flows using explicit this.Close() and return sequences inside validation blocks to prevent illegal execution paths.

---
+++ DVLD_UI/Licenses/Local Licenses/frmIssueDriverLicenseFirstTime.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace DVLD_UI.Licenses.Local_Licenses
+{
+    partial class frmIssueDriverLicenseFirstTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmIssueDriverLicenseFirstTime));
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.txt_Notes = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ctrlDrivingLicenseApplicationInfo1 = new DVLD_UI.Applications.Local_Driving_License.ctrlDrivingLicenseApplicationInfo();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Crimson;
+            this.btn_Close.CausesValidation = false;
+            this.btn_Close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Close.ImageIndex = 1;
+            this.btn_Close.ImageList = this.imageList1;
+            this.btn_Close.Location = new System.Drawing.Point(636, 684);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(102, 46);
+            this.btn_Close.TabIndex = 2;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "save.png");
+            this.imageList1.Images.SetKeyName(1, "close_2.png");
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.BackColor = System.Drawing.Color.Green;
+            this.btn_Save.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Save.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btn_Save.ForeColor = System.Drawing.Color.White;
+            this.btn_Save.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Save.ImageIndex = 0;
+            this.btn_Save.ImageList = this.imageList1;
+            this.btn_Save.Location = new System.Drawing.Point(746, 684);
+            this.btn_Save.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(102, 46);
+            this.btn_Save.TabIndex = 1;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Save.UseVisualStyleBackColor = false;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // txt_Notes
+            // 
+            this.txt_Notes.Location = new System.Drawing.Point(156, 508);
+            this.txt_Notes.Multiline = true;
+            this.txt_Notes.Name = "txt_Notes";
+            this.txt_Notes.Size = new System.Drawing.Size(692, 169);
+            this.txt_Notes.TabIndex = 212;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 508);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 24);
+            this.label4.TabIndex = 211;
+            this.label4.Text = "Notes : ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DVLD_UI.Properties.Resources.Notes_32;
+            this.pictureBox1.Location = new System.Drawing.Point(112, 504);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(31, 33);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 210;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ctrlDrivingLicenseApplicationInfo1
+            // 
+            this.ctrlDrivingLicenseApplicationInfo1.Location = new System.Drawing.Point(10, 21);
+            this.ctrlDrivingLicenseApplicationInfo1.Name = "ctrlDrivingLicenseApplicationInfo1";
+            this.ctrlDrivingLicenseApplicationInfo1.Size = new System.Drawing.Size(849, 481);
+            this.ctrlDrivingLicenseApplicationInfo1.TabIndex = 0;
+            // 
+            // frmIssueDriverLicenseFirstTime
+            // 
+            this.AcceptButton = this.btn_Save;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Close;
+            this.ClientSize = new System.Drawing.Size(871, 774);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.txt_Notes);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ctrlDrivingLicenseApplicationInfo1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmIssueDriverLicenseFirstTime";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmIssueDriverLicenseFirstTime";
+            this.Load += new System.EventHandler(this.frmIssueDriverLicenseFirstTime_Load);
+            this.Shown += new System.EventHandler(this.frmIssueDriverLicenseFirstTime_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Applications.Local_Driving_License.ctrlDrivingLicenseApplicationInfo ctrlDrivingLicenseApplicationInfo1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.TextBox txt_Notes;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ DVLD_UI/Licenses/Local Licenses/frmIssueDriverLicenseFirstTime.cs
@@ -0,0 +1,107 @@
+﻿using DVLD_Business;
+using DVLD_Model;
+using DVLD_UI.GlobalClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Licenses.Local_Licenses
+{
+    public partial class frmIssueDriverLicenseFirstTime : Form
+    {
+        private int _LocalDrivingLicenseApplicationID = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        private clsLocalDrivingLicenseApplications _localDrivingLicenseApplications = null;
+        private clsLicenses _Licenses = null;
+        public frmIssueDriverLicenseFirstTime(int LocalDrivingLicenseApplicationID)
+        {
+            InitializeComponent();
+            _LocalDrivingLicenseApplicationID = LocalDrivingLicenseApplicationID;
+        }
+        private void _initialize_localDrivingLicenseApplication()
+        {
+            _localDrivingLicenseApplications = clsLocalDrivingLicenseApplications.FindByLocalDrivingLicenseApplicationID(_LocalDrivingLicenseApplicationID);
+        }
+        private void SetLicenseInfo()
+        {
+            _Licenses = new clsLicenses();
+            _Licenses.LicenseClassID = _localDrivingLicenseApplications.LicenseClassID;
+            _Licenses.ApplicationID = _localDrivingLicenseApplications.ApplicationID;
+            _Licenses.IssueDate = DateTime.Now;
+            clsLicenseClasses clsLicenseClasses = clsLicenseClasses.Find(_localDrivingLicenseApplications.LicenseClassID);
+            _Licenses.ExpirationDate = DateTime.Now.AddYears(clsLicenseClasses.DefaultValidityLength);
+            _Licenses.IssueReason = clsLicenseModel.enIssueReason.FirstTime;
+            _Licenses.Notes = txt_Notes.Text.Trim();
+            _Licenses.PaidFees = clsLicenseClasses.ClassFees;
+            _Licenses.IsActive = true;
+            _Licenses.CreatedByUserID = clsGlobal.CurrentUser.UserID;  
+        }
+
+        private bool _IsLicenseExist()
+        {
+            return clsLicenses.IsLicenseExist(_localDrivingLicenseApplications.ApplicantPersonID, _localDrivingLicenseApplications.LicenseClassID);
+        }
+        private bool _IsPassedAllTests()
+        {
+            return clsTests.IsAllTestsPassed(_LocalDrivingLicenseApplicationID);
+        }
+        private void frmIssueDriverLicenseFirstTime_Load(object sender, EventArgs e)
+        {
+            _initialize_localDrivingLicenseApplication();
+
+            if (!_IsPassedAllTests())
+            {
+                MessageBox.Show("Cannot issue Driver License. Applicant has not passed all required tests.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+
+            if (_localDrivingLicenseApplications != null)
+            {
+                if (_IsLicenseExist())
+                {
+                    MessageBox.Show("A Driver License already exists for this applicant and license class.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.Close();
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Failed to load Local Driving License Application information.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.Close();
+                return;
+            }
+            ctrlDrivingLicenseApplicationInfo1.Load_LocalDrivingLicenseApplicationInfo(_LocalDrivingLicenseApplicationID);
+
+        }
+
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+
+            SetLicenseInfo();
+
+            if (_Licenses.Save())
+            {
+                MessageBox.Show("License Issued Successfully with License ID = " + _Licenses.LicenseID,
+                                   "Succeeded", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.Close();
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Failed to issue Driver License.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void frmIssueDriverLicenseFirstTime_Shown(object sender, EventArgs e)
+        {
+            txt_Notes.Focus();
+        }
+    }
+}
