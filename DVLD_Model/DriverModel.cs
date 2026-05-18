99fb8c1abfa8a5c11fb51276dc7749a466863b56
abdalbaset <abdallbaset2004@gmail.com>
2026-05-18 17:48:45 +0330

    feat(Model): add clsDriverModel to define driver data structure
    Description:
    
    Created clsDriverModel class within the Model layer to strictly handle driver-related data fields (DriverID, PersonID, CreatedByUserID, CreatedDate).

---
+++ DVLD_Model/DriverModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Model
+{
+    public class clsDriverModel
+    {
+
+        public int DriverID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public int PersonID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public int CreatedByUserID { get; set; } = (int)clsEnumerationsModel.enIdentityStatus.NonExistent;
+        public DateTime CreatedDate { get; set; } = DateTime.MinValue;
+    }
+}
