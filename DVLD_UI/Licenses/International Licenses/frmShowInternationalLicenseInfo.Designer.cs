521f2f8f1b40dd702817c8d24e60a46490602a29
abdalbaset <abdallbaset2004@gmail.com>
2026-06-04 21:47:23 +0330

    feat(ui): Add frmShowInternationalLicenseInfo form
    Description:
    
    Introduced the 'frmShowInternationalLicenseInfo' dialogue form container. This window acts as the presentation host layer, capturing context identifiers via instantiation parameters to fire data loading mechanisms in the embedded international license profile control.

---
+++ DVLD_UI/Licenses/International Licenses/frmShowInternationalLicenseInfo.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DVLD_UI.Licenses.International_Licenses
+{
+    partial class frmShowInternationalLicenseInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmShowInternationalLicenseInfo));
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ctrlDriverInternationalLicenseInfo1 = new DVLD_UI.Licenses.International_Licenses.Controls.ctrlDriverInternationalLicenseInfo();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.BackColor = System.Drawing.Color.Crimson;
+            this.btn_Close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Close.ImageIndex = 0;
+            this.btn_Close.ImageList = this.imageList1;
+            this.btn_Close.Location = new System.Drawing.Point(930, 513);
+            this.btn_Close.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(120, 48);
+            this.btn_Close.TabIndex = 20;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Close.UseVisualStyleBackColor = false;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Image = global::DVLD_UI.Properties.Resources.LicenseView_400;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1073, 108);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 20.2F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(-2, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1075, 56);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Driver International License Info";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ctrlDriverInternationalLicenseInfo1
+            // 
+            this.ctrlDriverInternationalLicenseInfo1.Location = new System.Drawing.Point(12, 197);
+            this.ctrlDriverInternationalLicenseInfo1.Name = "ctrlDriverInternationalLicenseInfo1";
+            this.ctrlDriverInternationalLicenseInfo1.Size = new System.Drawing.Size(1057, 309);
+            this.ctrlDriverInternationalLicenseInfo1.TabIndex = 21;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DVLD_UI.Properties.Resources.globe;
+            this.pictureBox2.Location = new System.Drawing.Point(432, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 22;
+            this.pictureBox2.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "close_2.png");
+            // 
+            // frmShowInternationalLicenseInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Close;
+            this.ClientSize = new System.Drawing.Size(1073, 582);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.ctrlDriverInternationalLicenseInfo1);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmShowInternationalLicenseInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "International Driver Info";
+            this.Load += new System.EventHandler(this.frmShowInternationalLicenseInfo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private Controls.ctrlDriverInternationalLicenseInfo ctrlDriverInternationalLicenseInfo1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
---
+++ DVLD_UI/Licenses/International Licenses/frmShowInternationalLicenseInfo.cs
@@ -0,0 +1,33 @@
+﻿using DVLD_UI.GlobalClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD_UI.Licenses.International_Licenses
+{
+    public partial class frmShowInternationalLicenseInfo : Form
+    {
+        private int _InternationalLicenseID = (int)clsGlobal.enIdentityStatus.NonExistent;
+        public frmShowInternationalLicenseInfo(int InternationalLicenseID)
+        {
+            InitializeComponent();
+            _InternationalLicenseID = InternationalLicenseID;
+        }
+
+        private void frmShowInternationalLicenseInfo_Load(object sender, EventArgs e)
+        {
+            ctrlDriverInternationalLicenseInfo1.Load_InternationalLicenseInfoInfo(_InternationalLicenseID);
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
