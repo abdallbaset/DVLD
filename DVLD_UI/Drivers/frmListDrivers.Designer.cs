54c2de0b978d9cab3e0f2d4ace0cf60fc17bb4ac
abdalbaset <abdallbaset2004@gmail.com>
2026-06-04 22:19:13 +0330

    feat(ui): Add frmListDrivers form with dynamic context filtering .
    Description:
    Introduced the main driver asset directory form 'frmListDrivers'. It builds an in-memory database view filter layer using DataView utilities, optimizes input controls based on current field targets, and hooks menu handlers into downstream core sub-profiles.
    
    Key Changes:
    
    Drivers Management Interface: Created frmListDrivers.cs to bind data collections via clsDrivers.GetAllDrivers().
    
    Adaptive Search Layout: Implemented dynamic text input sizing configuration checks within cmb_AllFilter_SelectedIndexChanged to automatically scale mtxt_Value.Width up to 350px during broad string searches (FullName) for optimal user visibility.
    
    Granular Text Filters: Configured text filtering query expressions (_FilterResult) supporting numeric mapping criteria (DriverID, PersonID) and string matching parameters via the SQL LIKE wildcard operator.
    
    Safe Validation Entry: Constrained the mtxt_Value_KeyPress capture sequence to explicitly filter down inputs to digit values when active filter modes depend on strict backend integer keys.
    
    Context Interactivity: Wired relational event bindings (_ShowPersonDetails and _ShowPersonLicenseHistory) to forward current DataGridView cell selections to secondary deep-dive display controllers (frmPersonDetails, frmShowPersonLicenseHistory).

---
+++ DVLD_UI/Drivers/frmListDrivers.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace DVLD_UI.Drivers
+{
+    partial class frmListDrivers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmListDrivers));
+            this.lbl_NumberOfRecords = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmb_AllFilter = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgv_ListDrivers = new System.Windows.Forms.DataGridView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mtxt_Value = new System.Windows.Forms.MaskedTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showPersonDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showPersonLicenseHistoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ListDrivers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_NumberOfRecords
+            // 
+            this.lbl_NumberOfRecords.AutoSize = true;
+            this.lbl_NumberOfRecords.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lbl_NumberOfRecords.Location = new System.Drawing.Point(113, 801);
+            this.lbl_NumberOfRecords.Name = "lbl_NumberOfRecords";
+            this.lbl_NumberOfRecords.Size = new System.Drawing.Size(19, 24);
+            this.lbl_NumberOfRecords.TabIndex = 6;
+            this.lbl_NumberOfRecords.Text = "?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 801);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "# Records:";
+            // 
+            // cmb_AllFilter
+            // 
+            this.cmb_AllFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_AllFilter.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.cmb_AllFilter.FormattingEnabled = true;
+            this.cmb_AllFilter.ItemHeight = 21;
+            this.cmb_AllFilter.Location = new System.Drawing.Point(117, 313);
+            this.cmb_AllFilter.Name = "cmb_AllFilter";
+            this.cmb_AllFilter.Size = new System.Drawing.Size(207, 29);
+            this.cmb_AllFilter.TabIndex = 1;
+            this.cmb_AllFilter.SelectedIndexChanged += new System.EventHandler(this.cmb_AllFilter_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Filter By:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(1, 199);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1313, 56);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Manage Drivers";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dgv_ListDrivers
+            // 
+            this.dgv_ListDrivers.AllowUserToAddRows = false;
+            this.dgv_ListDrivers.AllowUserToDeleteRows = false;
+            this.dgv_ListDrivers.AllowUserToOrderColumns = true;
+            this.dgv_ListDrivers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_ListDrivers.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv_ListDrivers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv_ListDrivers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ListDrivers.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgv_ListDrivers.Location = new System.Drawing.Point(12, 347);
+            this.dgv_ListDrivers.Name = "dgv_ListDrivers";
+            this.dgv_ListDrivers.ReadOnly = true;
+            this.dgv_ListDrivers.RowHeadersWidth = 51;
+            this.dgv_ListDrivers.RowTemplate.Height = 26;
+            this.dgv_ListDrivers.Size = new System.Drawing.Size(1302, 438);
+            this.dgv_ListDrivers.TabIndex = 3;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "close.png");
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btn_Close.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.btn_Close.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btn_Close.ImageIndex = 0;
+            this.btn_Close.ImageList = this.imageList1;
+            this.btn_Close.Location = new System.Drawing.Point(1191, 791);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(123, 45);
+            this.btn_Close.TabIndex = 4;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DVLD_UI.Properties.Resources.Driver_Main;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1313, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mtxt_Value
+            // 
+            this.mtxt_Value.Font = new System.Drawing.Font("Tahoma", 11F);
+            this.mtxt_Value.Location = new System.Drawing.Point(330, 313);
+            this.mtxt_Value.Name = "mtxt_Value";
+            this.mtxt_Value.Size = new System.Drawing.Size(263, 30);
+            this.mtxt_Value.TabIndex = 2;
+            this.mtxt_Value.TextChanged += new System.EventHandler(this.mtxt_Value_TextChanged);
+            this.mtxt_Value.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.mtxt_Value_KeyPress);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showPersonDetailsToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.showPersonLicenseHistoryToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(269, 62);
+            // 
+            // showPersonDetailsToolStripMenuItem
+            // 
+            this.showPersonDetailsToolStripMenuItem.Image = global::DVLD_UI.Properties.Resources.person_card_details;
+            this.showPersonDetailsToolStripMenuItem.Name = "showPersonDetailsToolStripMenuItem";
+            this.showPersonDetailsToolStripMenuItem.Size = new System.Drawing.Size(268, 26);
+            this.showPersonDetailsToolStripMenuItem.Text = "Show Person Details";
+            this.showPersonDetailsToolStripMenuItem.Click += new System.EventHandler(this.showPersonDetailsToolStripMenuItem_Click);
+            // 
+            // showPersonLicenseHistoryToolStripMenuItem
+            // 
+            this.showPersonLicenseHistoryToolStripMenuItem.Image = global::DVLD_UI.Properties.Resources.PersonLicenseHistory_5121;
+            this.showPersonLicenseHistoryToolStripMenuItem.Name = "showPersonLicenseHistoryToolStripMenuItem";
+            this.showPersonLicenseHistoryToolStripMenuItem.Size = new System.Drawing.Size(268, 26);
+            this.showPersonLicenseHistoryToolStripMenuItem.Text = "Show Person License History";
+            this.showPersonLicenseHistoryToolStripMenuItem.Click += new System.EventHandler(this.showPersonLicenseHistoryToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(265, 6);
+            // 
+            // frmListDrivers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btn_Close;
+            this.ClientSize = new System.Drawing.Size(1326, 867);
+            this.Controls.Add(this.mtxt_Value);
+            this.Controls.Add(this.lbl_NumberOfRecords);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmb_AllFilter);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_Close);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgv_ListDrivers);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmListDrivers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "List Drivers";
+            this.Load += new System.EventHandler(this.frmListDrivers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ListDrivers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_NumberOfRecords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmb_AllFilter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridView dgv_ListDrivers;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.MaskedTextBox mtxt_Value;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showPersonDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showPersonLicenseHistoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+    }
+}
---
+++ DVLD_UI/Drivers/frmListDrivers.cs
@@ -0,0 +1,192 @@
+﻿using DVLD_Business;
+using DVLD_UI.GlobalClasses;
+using DVLD_UI.Licenses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static DVLD_Model.clsEnumerationsModel;
+
+namespace DVLD_UI.Drivers
+{
+    public partial class frmListDrivers : Form
+    {
+        enum enFilterMode { None, DriverID, PersonID, NationalNo, FullName }
+        enFilterMode _FilterMode;
+        private DataView _ListDrivers;
+        public frmListDrivers()
+        {
+            InitializeComponent();
+        }
+        private void _InitializeFilterComboBox()
+        {
+            cmb_AllFilter.Items.Clear();
+            cmb_AllFilter.Items.AddRange(new object[] { "None", "Driver ID", "Person ID", "National No", "Full Name" });
+            cmb_AllFilter.SelectedIndex = cmb_AllFilter.FindString("None");
+        }
+
+        private void _RefreshListDrivers()
+        {
+            _ListDrivers = new DataView( clsDrivers.GetAllDrivers());
+            if(_ListDrivers.Count == 0)
+            {
+                MessageBox.Show("No drivers found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                dgv_ListDrivers.DataSource = null;
+                lbl_NumberOfRecords.Text = "0 Records";
+                return;
+            }
+            dgv_ListDrivers.DataSource = _ListDrivers;
+            _FormatDataGridViewColumn();
+            clsUtil.UpdateRecordCount(lbl_NumberOfRecords, _ListDrivers);
+        }
+
+        private void _FormatDataGridViewColumn()
+        {
+            if (dgv_ListDrivers.Rows.Count > 0)
+            {
+                clsUtil.ConfigureColumn(dgv_ListDrivers.Columns["DriverID"], "Driver ID", 100);
+                clsUtil.ConfigureColumn(dgv_ListDrivers.Columns["PersonID"], "Person ID", 100);
+                clsUtil.ConfigureColumn(dgv_ListDrivers.Columns["NationalNo"], "National No", 120);
+                clsUtil.ConfigureColumn(dgv_ListDrivers.Columns["FullName"], "Full Name", 200);
+                clsUtil.ConfigureColumn(dgv_ListDrivers.Columns["CreatedDate"], "Date", 100);
+                clsUtil.ConfigureColumn(dgv_ListDrivers.Columns["NumberOfActiveLicenses"], "Active Licenses", 200);
+            }
+        }
+        private void _FilterResult()
+        {
+            string filterExpression = "";
+
+            if (string.IsNullOrWhiteSpace(mtxt_Value.Text) && mtxt_Value.Visible)
+            {
+                _ListDrivers.RowFilter = "";
+                return;
+            }
+
+            string filterValue = mtxt_Value.Text.Trim().Replace("'", "''");
+
+            switch (_FilterMode)
+            {
+                case enFilterMode.DriverID:
+                case enFilterMode.PersonID:
+                    filterExpression = $"{_FilterMode} = {filterValue}";
+                    break;
+
+                case enFilterMode.NationalNo:
+                case enFilterMode.FullName:
+                    filterExpression = $"{_FilterMode} LIKE '%{filterValue}%'";
+                    break;
+
+                default:
+                    filterExpression = "";
+                    break;
+            }
+
+            _ListDrivers.RowFilter = filterExpression;
+        }
+        private void frmListDrivers_Load(object sender, EventArgs e)
+        {
+            _InitializeFilterComboBox();
+            _RefreshListDrivers();
+        }
+
+        private void cmb_AllFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _FilterMode = (enFilterMode)cmb_AllFilter.SelectedIndex;
+
+            if (_FilterMode == enFilterMode.None)
+            {
+                _RefreshListDrivers();
+                mtxt_Value.Visible = false;
+            }
+            else
+            {
+                mtxt_Value.Visible = true;
+                mtxt_Value.Clear();
+                mtxt_Value.Focus();
+
+                if(_FilterMode == enFilterMode.FullName)
+                    mtxt_Value.Width = 350;
+                else
+                    mtxt_Value.Width = 260;
+            }
+        }
+
+        private void mtxt_Value_TextChanged(object sender, EventArgs e)
+        {
+            _FilterResult();
+            clsUtil.UpdateRecordCount(lbl_NumberOfRecords, _ListDrivers);
+        }
+
+        private void mtxt_Value_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (_FilterMode == enFilterMode.DriverID || _FilterMode == enFilterMode.PersonID || _FilterMode == enFilterMode.NationalNo)
+            {
+                if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void _ErrorMessageWhenNoRowIsSelected()
+        {
+            MessageBox.Show("Please select a License first!", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+        private void _ShowPersonDetails()
+        {
+            if (dgv_ListDrivers.Rows.Count > 0 && dgv_ListDrivers.CurrentRow != null)
+            {
+
+                int PersonID = Convert.ToInt32(dgv_ListDrivers.CurrentRow.Cells["PersonID"].Value);
+
+                frmPersonDetails frmPersonDetails = new frmPersonDetails(PersonID);
+                frmPersonDetails.ShowDialog();
+                _RefreshListDrivers();
+                mtxt_Value.Clear();
+
+            }
+            else
+            {
+                _ErrorMessageWhenNoRowIsSelected();
+            }
+
+        }
+        private void _ShowPersonLicenseHistory()
+        {
+            if (dgv_ListDrivers.Rows.Count > 0 && dgv_ListDrivers.CurrentRow != null)
+            {
+                int PersonID = Convert.ToInt32(dgv_ListDrivers.CurrentRow.Cells["PersonID"].Value);
+         
+                frmShowPersonLicenseHistory frmShowPersonLicenseHistory = new frmShowPersonLicenseHistory(PersonID);
+                frmShowPersonLicenseHistory.ShowDialog();
+                _RefreshListDrivers();
+                mtxt_Value.Clear();
+
+            }
+            else
+            {
+                _ErrorMessageWhenNoRowIsSelected();
+            }
+
+        }
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void showPersonDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _ShowPersonDetails();
+        }
+
+        private void showPersonLicenseHistoryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _ShowPersonLicenseHistory();
+        }
+    }
+}
